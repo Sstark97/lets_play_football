f5cbd1c582fc50671d891fbb1e1ab8978a96895e
Sstark97 <aitorscinfo@gmail.com>
2023-02-11 12:57:04 +0000

    Change some width

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -6,7 +6,9 @@ const Player = ({ name, lastname, photo, position }: Player) => {
 
   return (
     <Link to={`player?name=${name}`} className="flex p-3 lg:w-1/3 lg:px-8 lg:py-0 lg:pt-3">
-        <img className="object-cover w-2/5 mr-8" src={photo} alt={name} />
+        <div className="w-2/5 mr-8 bg-neutral-900">
+          <img className="w-full text-white text-center object-cover" src={photo} alt={name} />
+        </div>
         <div className="mt-2">
             <p className="text-xl font-semibold lg:text-lg">{`${name} ${lastname}`}</p>
             <p className="text-lg font-semibold text-slate-400 lg:text-sm">{positionCapitalized}</p>
