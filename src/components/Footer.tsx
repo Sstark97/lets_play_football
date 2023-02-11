367fdf18c85cd5d9dd5e976b803b48976eacc86b
Sstark97 <aitorscinfo@gmail.com>
2023-02-11 12:13:41 +0000

    Fix Footer in player

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
+import { useLocation } from "react-router-dom"
+
 const Footer = () => {
+  const location = useLocation()
+  const position = location.pathname === "/player" ? "absolute bottom-0" : ""
+
   return (
-    <footer className="w-screen flex justify-center text-sm text-paris opacity-90 py-6 lg:p-0 lg:pb-6 lg:absolute lg:bottom-0">Me gusta el fútbol • 2023 ©</footer>
+    <footer className={`w-screen flex justify-center text-sm text-paris opacity-90 py-6 lg:p-0 lg:pb-6 lg:absolute lg:bottom-0 ${position}`}>Me gusta el fútbol • 2023 ©</footer>
   )
 }
 
