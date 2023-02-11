4117039656930489218d27b98b1328aa0740b33a
Sstark97 <aitorscinfo@gmail.com>
2023-02-11 12:19:17 +0000

    Add Link to Teams and Home

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
 import { RiFootballLine } from "react-icons/ri"
 
@@ -9,12 +9,12 @@ const Header = () => {
   return (
     <header className="w-screen h-20 text-paris text-lg lg:h-16">
       <nav className="w-full h-full flex border-b-[1px] border-paris">
-        <div className="w-1/5 flex items-center justify-center lg:w-[6%]">
+        <Link to={"/"} className="w-1/5 flex items-center justify-center lg:w-[6%]">
           <RiFootballLine className="text-4xl fill-white bg-paris border-4 border-paris rounded-xl"/>
-        </div>
+        </Link>
         <div className="w-3/5 flex justify-around items-center border-x-[1px] border-paris lg:w-[88%] lg:justify-start">
           <div className={`font-semibold lg:mx-16 ${isPlayerPath ? "text-active" : ""}`}>PLAYERS</div>
-          <div className={`font-semibold ${!isPlayerPath ? "text-active" : ""}`}>TEAMS</div>
+          <Link to={"/"} className={`font-semibold ${!isPlayerPath ? "text-active" : ""}`}>TEAMS</Link>
         </div>
         <div className="w-1/5 flex items-center justify-center lg:w-[8%]">
           <AiOutlineSearch className="text-2xl"/>
