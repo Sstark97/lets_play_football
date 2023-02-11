7b53e3b7a29ae7fa6e02d7c7efa66a598b7d5b3f
Sstark97 <aitorscinfo@gmail.com>
2023-02-11 21:33:43 +0000

    Fix break img

--- src/components/PlayerImg.tsx
+++ src/components/PlayerImg.tsx
@@ -3,11 +3,19 @@ import usePlayer from "@hooks/usePlayer"
 const PlayerImg = () => {
   const { player } = usePlayer()
   const { name, photo, logo } = player
-  
+
   return (
     <div className="flex justify-center bg-neutral-900 relative lg:w-[32%] lg:h-[32vh] lg:mt-4">
-        <img className="w-full h-full" src={photo} alt={name} />
-        <img className="absolute w-[15%] bottom-3 right-5" src={logo} alt={name}/>
+      <img
+        className="w-full h-full"
+        src={photo}
+        alt={name}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null
+          currentTarget.src = `https://fakeimg.pl/1765x994/000/?text=${name}`
+        }}
+      />
+      <img className="absolute w-[15%] bottom-3 right-5" src={logo} alt={name} />
     </div>
   )
 }
